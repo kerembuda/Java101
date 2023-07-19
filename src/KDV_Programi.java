5506bb8d2322467797b51b7c8834760ae9ad1543
Kerem Buda <kerembuda98@gmail.com>
2023-07-19 21:20:30 +0300

    Added one missing

---
+++ src/KDV_Programi.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class KDV_Programi {
+    public static void main (String[] args) {
+                Scanner input = new Scanner(System.in);
+                System.out.print("Para miktarını yazınız");
+
+                double para = input.nextDouble();
+                double kdv = para * 0.18;
+
+                System.out.println("Paranız: " + para);
+                System.out.println("Kdv Miktarı: " + kdv);
+                System.out.println("Kdv'li genel toplam: " + (para + kdv));
+    }
+}
