aeac147353cdf3aed37183715f7171692ed31f57
Kerem Buda <kerembuda98@gmail.com>
2023-07-24 22:45:05 +0300

    Added Min_Max.java project.

---
+++ src/Min_Max.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Min_Max {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int numCount, num, tempNum;
+        int min = 0;
+        int max = 0;
+
+        System.out.print("How many numbers are you going to add? :");
+        numCount = input.nextInt();
+
+        for (int i = 1; i <= numCount; i++) {
+            System.out.print("Enter " + i + ". number: ");
+            num = input.nextInt();
+            if (i == 1) {
+                min = num;
+                max = num;
+            }
+
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+
+            }
+        }
+        System.out.println("Minimum number is: " + min +"\nMaximum number is: " + max);
+    }
+}
