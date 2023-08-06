a9c0aec6e32e573c95ddab128d5af751d7bca10f
Kerem Buda <kerembuda98@gmail.com>
2023-08-06 17:07:16 +0300

    made some adjustments

--- src/KDV_Programi.java
+++ src/KDV_Programi.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 public class KDV_Programi {
     public static void main (String[] args) {
                 Scanner input = new Scanner(System.in);
-                System.out.print("Para miktarını yazınız");
+                System.out.print("Para miktarını yazınız: ");
 
                 double para = input.nextDouble();
-                double kdv = para * 0.18;
+                double kdv = para * 0.20;
 
                 System.out.println("Paranız: " + para);
                 System.out.println("Kdv Miktarı: " + kdv);
