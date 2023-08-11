ea5c60d24c3b5dc6812fb0845f4da869150ae964
Kerem Buda <kerembuda98@gmail.com>
2023-08-11 21:58:36 +0300

    made some adjustments

--- src/Boxing_Game/Main.java
+++ src/Boxing_Game/Main.java
@@ -2,7 +2,7 @@ package Boxing_Game;
 
 public class Main {
     public static void main(String[] args) {
-        Fighter f1 = new Fighter("Hasbulla", 10, 100,50, 45,10);
+        Fighter f1 = new Fighter("Hasbulla", 18, 100,50, 75,10);
         Fighter f2 = new Fighter("Yiğit",20,125,55, 15, 50);
 
         Match UFC = new Match(f1,f2);
