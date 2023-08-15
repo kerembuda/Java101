4f907f4caf167f2d4ff99b67164249298c018df7
Kerem Buda <kerembuda98@gmail.com>
2023-08-15 09:49:08 +0300

    Finished boxing game i guess works well

--- src/Boxing_Game/Main.java
+++ src/Boxing_Game/Main.java
@@ -3,7 +3,7 @@ package Boxing_Game;
 public class Main {
     public static void main(String[] args) {
         Fighter f1 = new Fighter("Hasbulla", 10, 100,50, 45,10);
-        Fighter f2 = new Fighter("Yiğit",20,125,55, 15, 50);
+        Fighter f2 = new Fighter("Göko",20,125,55, 15, 50);
 
         Match UFC = new Match(f1,f2);
         UFC.game_run();
