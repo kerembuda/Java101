eb764bfc442012f21ddfd92df2b62b8c5b1edfd0
Kerem Buda <kerembuda98@gmail.com>
2023-09-04 22:04:04 +0300

    made some adjustments

---
+++ src/Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[][] matris = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+                {10, 11, 12}
+        };
+
+    }
+}
