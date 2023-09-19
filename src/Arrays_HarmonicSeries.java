f5a65f20a818c828c22221e00d43fadcef91178c
Kerem Buda <kerembuda98@gmail.com>
2023-09-19 11:33:22 +0300

    updates

--- src/Arrays_HarmonicSeries.java
+++ src/Arrays_HarmonicSeries.java
@@ -1,5 +1,14 @@
 public class Arrays_HarmonicSeries {
     public static void main(String[] args) {
+        int[] numbers_array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 72,15,};
+        double sum = 0;
+        double result = 0;
+        for (int i = 0;i < numbers_array.length; i++) {
+            sum += numbers_array[i];
+            result += (1.0/numbers_array[i]);
+        }
 
+        System.out.println("Average: "+ sum /numbers_array.length);
+        System.out.println("Harmonic series: " + result);
     }
 }
