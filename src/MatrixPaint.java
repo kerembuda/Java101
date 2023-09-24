7468f7fa9206dbb26b5452f2da823a7c6a13d430
Kerem Buda <kerembuda98@gmail.com>
2023-09-24 17:44:06 +0300

    added matrix painter

--- src/MatrixPaint.java
+++ src/MatrixPaint.java
@@ -1,7 +1,7 @@
 public class MatrixPaint {
     public static void main(String[] args) {
-        String[][] matrix = new String[8][11];
-
+        String[][] matrix = new String[5][11];
+// do not change 11 otherwise it will break
         //initialize the array dudes in to place
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
@@ -11,12 +11,12 @@ public class MatrixPaint {
                     matrix[i][j] = "*";
                 } else if (i > 1 && j == 10) {
                     matrix[i][j] = "*";
-                } else if (i == 0) {
+                } else if (i == 0 && ( j == 2 || j == 3 || j == 7 || j == 8) ) {
                     matrix[i][2] = "*";
                     matrix[i][3] = "*";
                     matrix[i][7] = "*";
                     matrix[i][8] = "*";
-                } else if (i == 1) {
+                } else if (i == 1 && (j == 1 || j == 4 || j == 6 || j == 9)) {
                     matrix[i][1] = "*";
                     matrix[i][4] = "*";
                     matrix[i][6] = "*";
