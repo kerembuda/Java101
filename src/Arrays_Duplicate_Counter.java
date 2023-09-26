370e34e9f3c84bd265fe70634a1633db6968506d
Kerem Buda <kerembuda98@gmail.com>
2023-09-26 16:26:25 +0300

    updates

--- src/Arrays_Duplicate_Counter.java
+++ src/Arrays_Duplicate_Counter.java
@@ -3,8 +3,22 @@ public class Arrays_Duplicate_Counter {
         int[] list = {10, 15, 10, 20, 16, 10, 15, 20, 25, 50};
         //bence bi void fonksyon içinde sayıp bastırabiliriz her spesifik elemanı içine alan bir parametre,
         //sonra for loop ile her elemanı ayrıca sayabiliriz, içine if conditionu verebiliriz?
-        // bu elemanı bi kere sorduysan
+        // bu elemanı bi kere sorsduysan
         //sorma diye ama onu nasıl yapcaz
+        System.out.println(appearanceCount(list,1));
+
 
     }
+
+    public static int appearanceCount(int[] arr, int index) {
+        int counter = 0;
+        int j = arr[index];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == j) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
 }
