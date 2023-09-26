7af44c6709e99cb7c85703579270792ae0763fa1
Kerem Buda <kerembuda98@gmail.com>
2023-09-26 16:02:03 +0300

    updates

---
+++ src/Arrays_Duplicate_Counter.java
@@ -0,0 +1,10 @@
+public class Arrays_Duplicate_Counter {
+    public static void main(String[] args) {
+        int[] list = {10, 15, 10, 20, 16, 10, 15, 20, 25, 50};
+        //bence bi void fonksyon içinde sayıp bastırabiliriz her spesifik elemanı içine alan bir parametre,
+        //sonra for loop ile her elemanı ayrıca sayabiliriz, içine if conditionu verebiliriz?
+        // bu elemanı bi kere sorduysan
+        //sorma diye ama onu nasıl yapcaz
+
+    }
+}
