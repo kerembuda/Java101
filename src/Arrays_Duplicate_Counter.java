e387ffa2ba67bc8f1bed3092355526ad2175dbd0
Kerem Buda <kerembuda98@gmail.com>
2023-09-26 16:35:14 +0300

    Added array element counter for all elements in method.

--- src/Arrays_Duplicate_Counter.java
+++ src/Arrays_Duplicate_Counter.java
@@ -1,15 +1,11 @@
 public class Arrays_Duplicate_Counter {
     public static void main(String[] args) {
         int[] list = {10, 15, 10, 20, 16, 10, 15, 20, 25, 50};
-        //bence bi void fonksyon içinde sayıp bastırabiliriz her spesifik elemanı içine alan bir parametre,
-        //sonra for loop ile her elemanı ayrıca sayabiliriz, içine if conditionu verebiliriz?
-        // bu elemanı bi kere sorsduysan
-        //sorma diye ama onu nasıl yapcaz
-        System.out.println(appearanceCount(list,1));
 
-
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[i] +" appeared "+appearanceCount(list, i) + " times in list.");
+        }
     }
-
     public static int appearanceCount(int[] arr, int index) {
         int counter = 0;
         int j = arr[index];
