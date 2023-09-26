c9980ae7fcbe99641aab9e6e92c0f3ddff9382ea
Kerem Buda <kerembuda98@gmail.com>
2023-09-26 21:58:18 +0300

    update with big news

--- src/Arrays_Duplicate_Counter.java
+++ src/Arrays_Duplicate_Counter.java
@@ -1,11 +1,16 @@
 public class Arrays_Duplicate_Counter {
     public static void main(String[] args) {
         int[] list = {10, 15, 10, 20, 16, 10, 15, 20, 25, 50};
+        int[] printed_list = new int[list.length];
 
         for (int i = 0; i < list.length; i++) {
-            System.out.println(list[i] +" appeared "+appearanceCount(list, i) + " times in list.");
+            if (!contains(printed_list, list[i])) {
+                System.out.println(list[i] + " appeared " + appearanceCount(list, i) + " times in list.");
+                printed_list[i] = list[i];
+            }
         }
     }
+
     public static int appearanceCount(int[] arr, int index) {
         int counter = 0;
         int j = arr[index];
@@ -15,6 +20,15 @@ public class Arrays_Duplicate_Counter {
             }
         }
         return counter;
+    }
+
+    public static boolean contains(int[] arr, int value) {
+        for (int i = 0; i < arr.length; i++) {
+            if (value == arr[i]) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
