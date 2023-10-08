f73a99bd38c70af1d2f7b2c5e747d8aacf264829
Kerem Buda <kerembuda98@gmail.com>
2023-10-08 20:15:43 +0300

    Started working on minesweeper game

--- src/Mine_Sweeper_Game/Main.java
+++ src/Mine_Sweeper_Game/Main.java
@@ -1,14 +1,26 @@
 package Mine_Sweeper_Game;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        MineSweeper game = new MineSweeper();
-        int[][] board = game.create_board();
-        board = game.generate_mines(board);
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter table height: ");
+        int boardRow = input.nextInt();
+        System.out.print("Please enter table width: ");
+        int boardColumn = input.nextInt();
 
-        game.print_board(board);
+        MineSweeper game = new MineSweeper(boardRow,boardColumn);
+
+        int x = game.get_x();
+        int y = game.get_y();
+
+        if ((x > game.board_height && x < 0) && (y > game.board_width && y < 0)) {
+
+        }
+
+        game.get_Mine_Count_Around(x,y);
+        System.out.println(game.get_Mine_Count_Around(x,y));
     }
 }
--- src/Mine_Sweeper_Game/MineSweeper.java
+++ src/Mine_Sweeper_Game/MineSweeper.java
@@ -4,23 +4,29 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class MineSweeper {
-    public int[][] create_board() {
-        //Here we take column/row size from the user.
-        Scanner input = new Scanner(System.in);
-        System.out.print("Please enter table height: ");
-        int boardRow = input.nextInt();
-        System.out.print("Please enter table width: ");
-        int boardColumn = input.nextInt();
-        return new int[boardRow][boardColumn];
+    int board_height;
+    int board_width;
+    int[][] empty_board;
+    int[][] armed_board;
+
+
+    public MineSweeper(int board_height, int board_width) {
+        this.board_height = board_height;
+        this.board_width = board_width;
+        this.empty_board = new int[this.board_height][this.board_width];
+        this.armed_board = generate_mines(this.empty_board);
     }
+
 
     public int[][] generate_mines(int[][] empty_board) {
 
         //we take empty board, generate random locations for mines to be armed.
         //it gives back armed board.
-
         int height = empty_board.length;
         int width = empty_board[0].length;
+
+        int[][] armed_board = copy_array(empty_board);
+
         int table_slots_count = height * width;
         int mine_count = (int) (table_slots_count * 0.25);
         Random random = new Random();
@@ -28,23 +34,55 @@ public class MineSweeper {
 
         while (mine_counter <= mine_count) {
             int mine_arm_location = random.nextInt(table_slots_count);
-            if (empty_board[(int) mine_arm_location / width][(mine_arm_location % width)] == 0) {
-                empty_board[(int) mine_arm_location / width][(mine_arm_location % width)] = 9;
+            if (armed_board[(int) mine_arm_location / width][(mine_arm_location % width)] == 0) {
+                armed_board[(int) mine_arm_location / width][(mine_arm_location % width)] = 9;
                 mine_counter++;
             }
         }
-        return empty_board;
+        return armed_board;
     }
 
     public void print_board(int[][] board) {
-        for (int[] row: board) {
-            for (int col: row) {
+        for (int[] row : board) {
+            for (int col : row) {
                 System.out.print(col + " ");
             }
             System.out.println();
         }
     }
 
+    public int[][] copy_array(int[][] arr) {
+        int[][] copied_array = new int[arr.length][arr[0].length];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                copied_array[i][j] = arr[i][j];
+            }
+        }
+        return copied_array;
+    }
 
+
+    public int get_Mine_Count_Around(int x, int y) {
+        int counter = 0;
+
+        int current_location = this.armed_board[x][y];
+
+
+    return counter;
+    }
+
+    public int get_x() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please select the row. " + " 0 - " + (this.board_height) + " : ");
+        int x = input.nextInt();
+        return x;
+    }
+
+    public int get_y() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please select the column. " + " 0 - " + (this.board_width) + " : ");
+        int y = input.nextInt();
+        return y;
+    }
 }
 
